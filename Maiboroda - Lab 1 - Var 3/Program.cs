0e395bfc217873430c74056e3f6cffcc3882c3e1
maybo <maybo@DESKTOP-Q2ESNAL>
2022-09-12 18:55:22 +0200

    Добавьте файлы проекта.

---
+++ Maiboroda - Lab 1 - Var 3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Формула: G = n(y + 3.5) + корень из y");
+Console.WriteLine(" ");
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+Console.Write("Введите y: ");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+
+double G = n * (y + 3.5) + Math.Sqrt (y) ;
+Console.WriteLine($"{n} * ({y} + 3.5) + {Math.Sqrt (y):F3} = {G:F2}");
